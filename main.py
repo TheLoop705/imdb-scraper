7914c211ab13c194f0cc48c4d8e963e0b8729a27
TheLoop705 <sultan.d94@gmail.com>
2023-02-12 17:11:55 +0100

    list of dicts

--- main.py
+++ main.py
@@ -12,8 +12,20 @@ table = soup.find("table", class_="chart")
 # cols: 'Rank & Title',	'IMDb Rating', 'Your Rating'
 
 contents = []
+for tr in soup.find_all("tr"):
+    data = {}
+    try:
+        poster_column = tr.find("td", class_="posterColumn")
+        title_column = tr.find("td", class_="titleColumn")
+        rating_column = tr.find("td", class_="ratingColumn imdbRating")
 
-for row in table.tbody.find_all("td"):
-    cell = {}
-    # cell{'posterColumn'}= = row
-    print(row)
+        data["poster_src"] = poster_column.find("img").get("src")
+        data["title"] = title_column.find("a").text.strip()
+        data["imdb_rating"] = rating_column.find("strong").text
+
+        contents.append(data)
+    except AttributeError:
+        pass
+
+
+print(contents)
